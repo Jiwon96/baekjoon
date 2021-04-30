eb967812dabb0e74547155c988f3afe5195788f7
Jiwon96 <jiwon96@github.com>
2021-04-30 12:58:26 +0900

    4/30푸는중

--- bj2250/bj2250.cpp
+++ bj2250/bj2250.cpp
@@ -1,5 +1,48 @@
+#include<map>
 #include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+
 int main()
 {
+    map<int, pair<int, int>> t;
+    int arr[10001][2] ={0}; // min, max 구할 array
+    int n;
+    int a, b, c; //노드 입력 변수
+    stack < int > s;
     
+    cin >> n;
+
+    for(int i =1; i<=n; i++)
+    {
+        cin >> a >> b >> c;
+        t[a] = make_pair(b, c);
+    }
+    
+    int i =1;
+    while(1)
+    {
+        while(t[i].first != -1)
+        {
+            s.push(t[i].first);
+            i = t[i].first;
+        }           
+        while(t[i].first == -1)
+        {
+            arr[i][0] = i;
+            i=s.top();
+            s.pop();
+        }
+         
+
+
+        if(t[i].second == -1) continue;
+        
+        i++;
+    }
+
 }
