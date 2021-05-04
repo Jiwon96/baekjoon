14ffaf4cf4b4ffe54a9ad31731fcf7a19ad4e3fc
Jiwon96 <jiwon96@github.com>
2021-05-04 09:09:11 +0900

    저장 공간에 저장해놓으면 같은 root에 대해 연산을 2번하지 않는다 시간초과에서 탈출

--- bj1351.cpp
+++ bj1351.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
 #include<algorithm>
 #include<string.h>
-
+#include<map>
 using namespace std;
+
+map <long long, long long> ma;
 
 int P, Q;
 long long int N;
 long long int result=0;
-int recursive(int root)
+int recursive(long long root)
 {
-    if(root == 0)
-        return 1;
-    return recursive(root/P)+recursive(root/Q);
+    if(ma.count(root))
+        return ma[root];
+    else{
+        ma[root] =recursive(root/P)+recursive(root/Q);
+    }
+    return ma[N]; 
 }
 
 
